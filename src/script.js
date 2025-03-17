05b89a3657384b3a300ba286ed97ec3936420fa7
Anneke <122925139+anneke44@users.noreply.github.com>
2025-03-17 15:14:03 +0200

    Adding event listener to the input form "submt"

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function generateStory(event) {
+  event.preventDefault();
+  let shortStoryElement = document.querySelector("short-story");
+  shortStoryElement = "Story";
+}
+
+let formElement = document.querySelector("#input-form");
+formElement.addEventListener("submit", generateStory);
