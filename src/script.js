99296e86b0fec374fee6c9c5c164248b82c75077
Anneke <122925139+anneke44@users.noreply.github.com>
2025-03-17 18:42:16 +0200

    spacing adjustment in js

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generateStory(event) {
   let userInput = document.querySelector("#text-input");
   let apiKey = "04f3tf2c9f9bboc83b5050dcf54e2f1a";
   let context =
-    "you are an AI assistant writing unique short stories of no more than 40 words. The stories are short and impactful leaving a powerful image behind. Give your answer in HTML format with a <br/> at the end of every sentence. Let the first word of your answer be wrapped in a <strong></strong> element. Leave a signature of 'Your AI Friend' at the bottom, add a <br/><br/> in front of the signature and wrap in a <em></em> element.";
+    "you are an AI assistant writing unique short stories of no more than 40 words. The stories are short and impactful leaving a powerful image behind. Give your answer in HTML format with a <br/> at the end of every sentence. Let the first word of your answer be wrapped in a <strong></strong> element. Leave a signature of 'Your AI Friend' at the bottom, add a <br/> in front of the signature and wrap in a <em></em> element.";
   let prompt = `write me a story which is based or inpsired by ${userInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
